2cd759985a15e0344144b63902943fca18b6e9bb
NataliPerezG <nataperezg22@gmail.com>
2026-08-03 17:12:04 -0500

    project 2: sticky notes - workspace and create note styles

--- sticky-notes/src/main.js
+++ sticky-notes/src/main.js
@@ -1,1 +1,9 @@
 import './style.css'
+
+
+const bntAdd = document.querySelector('.btnAdd');
+const modalCreate = document.querySelector('.note__create')
+
+bntAdd.addEventListener('click', e => {
+    modalCreate.showModal()
+})
