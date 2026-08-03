9cf3b1a2763d0124033909c66b2a2dbbc9367720
NataliPerezG <nataperezg22@gmail.com>
2026-08-03 18:41:41 -0500

    project 2: sticky notes - initial styles finished

--- sticky-notes/src/main.js
+++ sticky-notes/src/main.js
@@ -3,7 +3,17 @@ import './style.css'
 
 const bntAdd = document.querySelector('.btnAdd');
 const modalCreate = document.querySelector('.note__create')
+const btnEdit = document.querySelector('.note__edit')
+const modalEdit = document.querySelector('.modal__edit')
+
 
 bntAdd.addEventListener('click', e => {
     modalCreate.showModal()
+})
+
+
+btnEdit.addEventListener('click', e => {
+    console.log('edit');
+    modalEdit.showModal()
+
 })
