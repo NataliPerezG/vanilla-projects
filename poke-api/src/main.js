68ca1d0b8385194516adae6f148ff385606b629a
NataliPerezG <nataliperezgarcia311@gmail.com>
2026-09-18 18:46:59 -0500

    first version completed

--- poke-api/src/main.js
+++ poke-api/src/main.js
@@ -1,60 +1,100 @@
-import './style.css'
-import heroImg from './assets/hero.png'
-import javascriptLogo from './assets/javascript.svg'
-import viteLogo from './assets/vite.svg'
-import { setupCounter } from './counter.js'
+import "./style.css";
 
-document.querySelector('#app').innerHTML = `
-<section id="center">
-  <div class="hero">
-    <img src="${heroImg}" class="base" width="170" height="179">
-    <img src="${javascriptLogo}" class="framework" alt="JavaScript logo"/>
-    <img src="${viteLogo}" class="vite" alt="Vite logo" />
-  </div>
-  <div>
-    <h1>Get started</h1>
-    <p>Edit <code>src/main.js</code> and save to test <code>HMR</code></p>
-  </div>
-  <button id="counter" type="button" class="counter"></button>
-</section>
+const URL_BASE = "https://pokeapi.co/api/v2/pokemon/";
+const length = 200;
 
-<div class="ticks"></div>
+const callPokeApi = async () => {
+  const allTypes = new Set();
 
-<section id="next-steps">
-  <div id="docs">
-    <svg class="icon" role="presentation" aria-hidden="true"><use href="/icons.svg#documentation-icon"></use></svg>
-    <h2>Documentation</h2>
-    <p>Your questions, answered</p>
-    <ul>
-      <li>
-        <a href="https://vite.dev/" target="_blank">
-          <img class="logo" src="${viteLogo}" alt="" />
-          Explore Vite
-        </a>
-      </li>
-      <li>
-        <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-          <img class="button-icon" src="${javascriptLogo}" alt="">
-          Learn more
-        </a>
-      </li>
-    </ul>
-  </div>
-  <div id="social">
-    <svg class="icon" role="presentation" aria-hidden="true"><use href="/icons.svg#social-icon"></use></svg>
-    <h2>Connect with us</h2>
-    <p>Join the Vite community</p>
-    <ul>
-      <li><a href="https://github.com/vitejs/vite" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#github-icon"></use></svg>GitHub</a></li>
-      <li><a href="https://chat.vite.dev/" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#discord-icon"></use></svg>Discord</a></li>
-      <li><a href="https://x.com/vite_js" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#x-icon"></use></svg>X.com</a></li>
-      <li><a href="https://bsky.app/profile/vite.dev" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#bluesky-icon"></use></svg>Bluesky</a></li>
-    </ul>
-  </div>
-</section>
+  const urls = Array.from({ length }, (_, i) => URL_BASE + (i + 1));
+  const promises = urls.map(async (url) => {
+    const resp = await fetch(url);
+    return resp.json();
+  });
+  const pokemonsData = await Promise.all(promises);
 
-<div class="ticks"></div>
-<section id="spacer"></section>
-`
+  const types = getTypes(pokemonsData, allTypes);
+  drawCategories(types);
 
-setupCounter(document.querySelector('#counter'))
+  drawCards(pokemonsData);
+
+  changeCategory(pokemonsData);
+};
+
+function getTypes(data, typeSet) {
+  data.forEach(({ types }) => {
+    types.forEach((type) => typeSet.add(type.type.name));
+  });
+  return [...typeSet];
+}
+
+function drawCategories(categories) {
+  const nav = document.querySelector(".categories");
+
+  categories.forEach((category) => {
+    const li = document.createElement("li");
+    li.classList.add("li-item");
+
+    const button = document.createElement("button");
+    button.classList.add("btn", category);
+    button.id = category;
+    button.textContent = category;
+
+    li.append(button);
+    nav.append(li);
+  });
+}
+
+function drawCards(data) {
+  const container = document.querySelector(".cards");
+  container.innerHTML = "";
+
+  const template = document.querySelector("#card-template").content;
+
+  data.forEach((pokemon) => {
+    const clone = template.cloneNode(true);
+
+    const id = pokemon.id;
+    const textId = id.toString().padStart(3, "0");
+
+    clone.querySelector("h2").textContent = `#${textId}`;
+    clone
+      .querySelector("img")
+      .setAttribute("src", pokemon.sprites.front_default);
+    clone.querySelector(".id-pok").textContent = `#${textId}`;
+    clone.querySelector(".name-pok").textContent = pokemon.name;
+
+    const categories = clone.querySelector(".categories");
+    pokemon.types.forEach((tipe) => {
+      const p = document.createElement("p");
+      p.textContent = tipe.type.name;
+      categories.append(p);
+    });
+
+    clone.querySelector(".weight").textContent = `WEIGHT: ${pokemon.weight}`;
+    clone.querySelector(".height").textContent = `HEIGHT: ${pokemon.height}`;
+    container.append(clone);
+  });
+}
+
+function changeCategory(data) {
+  const categories = document.querySelector(".categories");
+  categories.addEventListener("click", (e) => {
+    if (e.target.localName != "button") return;
+
+    const buttonId = e.target.id;
+
+    if (buttonId === "all") {
+      drawCards(data);
+      return;
+    }
+
+    const filteredPokemons = data.filter((pok) =>
+      pok.types.some((t) => t.type.name === buttonId),
+    );
+
+    drawCards(filteredPokemons);
+  });
+}
+
+callPokeApi();
